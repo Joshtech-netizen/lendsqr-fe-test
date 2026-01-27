c6c3c58c4db5533072b7617a3decce4a65af3632
Joshtech-netizen <kwakyejoshua14@gmail.com>
2026-01-27 14:42:52 +0000

    feat and style(navigation): implement top navbar with search and profile sections and apply flexbox layout and responsive search bar styling

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Navbar.module.scss';
+import logo from '../../assets/Group.svg';
+import { FaSearch, FaRegBell, FaCaretDown } from 'react-icons/fa';
+import profilePic from '../../assets/avatar.png'; 
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logoSection}>
+        <img src={logo} alt="Lendsqr" className={styles.logo} />
+      </div>
+
+      <div className={styles.searchBar}>
+        <input type="text" placeholder="Search for anything" />
+        <button id={styles.searchBtn}>
+          <FaSearch />
+        </button>
+      </div>
+
+      <div className={styles.profileSection}>
+        <a href="#" className={styles.docs}>Docs</a>
+        <FaRegBell className={styles.bellIcon} />
+        <div className={styles.user}>
+          <img src={profilePic} alt="Adedeji" className={styles.avatar} />
+          <span className={styles.name}>Adedeji</span>
+          <FaCaretDown className={styles.dropdownIcon} />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
