9ca334fe52549282917e7e4b9c800e9b2e75f15a
Joshtech-netizen <kwakyejoshua14@gmail.com>
2026-01-31 05:58:12 +0000

    refactor(types): align iuser interface with flat json mock data structure

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,44 +1,4 @@
-export type UserStatus = {
-  ACTIVE: "Active",
-  INACTIVE: "Inactive",
-  PENDING: "Pending",
-  BLACKLISTED: "Blacklisted",
-};
-
-export interface IProfile {
-  firstName: string;
-  lastName: string;
-  phoneNumber: string;
-  avatar: string;
-  gender: string;
-  bvn: string;
-  address: string;
-  currency: string;
-}
-
-export interface IEducation {
-  level: string;
-  employmentStatus: string;
-  sector: string;
-  duration: string;
-  officeEmail: string;
-  monthlyIncome: [string, string]; 
-  loanRepayment: string;
-}
-
-export interface ISocials {
-  facebook: string;
-  instagram: string;
-  twitter: string;
-}
-
-export interface IGuarantor {
-  firstName: string;
-  lastName: string;
-  phoneNumber: string;
-  gender: string;
-  relationship: string;
-}
+export type StatusType = "Active" | "Inactive" | "Pending" | "Blacklisted";
 
 export interface IUser {
   id: number; 
@@ -49,6 +9,6 @@ export interface IUser {
   createdAt: string;
   status: string;
   profile: string; 
-  education: string; 
-  guarantor: string; 
+  education: string;
+  guarantor: string;
 }
